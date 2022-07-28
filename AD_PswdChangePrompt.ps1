e9419a70a386a6c4ee0ca4183ef85746746342d3
AlCpwnd <a.dejaeger@hotmail.com>
2022-07-28 16:37:41 +0200

    Show prompt when user password is about to expire

---
+++ AD_PswdChangePrompt.ps1
@@ -0,0 +1,20 @@
+Add-Type -AssemblyName PresentationFramework
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+# Number of days before the expiration the prompt should show up
+$Limit = 20
+
+# Recovers current user's information
+$UserInfo = net user $env:USERNAME /domain
+# Isolates the line regarding the next password change
+$DateString = ($UserInfo | Where-Object{$_ -match "Password expires"}).split(" ")[-2]
+# Converts the isolated data to a valid DateTime format
+$DateLimit = [DateTime]::parseexact($DateString, 'dd/MM/yyyy', $null)
+# Calculates the time left before the change
+$Days = ($DateLimit - (Get-Date)).Days
+
+if($Days -le $Limit){
+    # Generates the prompt containing the information
+    [System.Windows.MessageBox]::Show("Your password expires in $Days days!`nPlsease use Ctrl+Alt+Del to change your password.","Password Expiration","Ok","Warning")
+}
